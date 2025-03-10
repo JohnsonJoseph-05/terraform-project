a04d08073ef50cf5f2bc38452c070f2616253862
JohnsonJoseph-05 <johnsonmadathikunnel@gmail.com>
2025-03-10 13:33:16 -0400

    Update variables.tf

--- modules/rgroup-n01698490/variables.tf
+++ modules/rgroup-n01698490/variables.tf
@@ -1,9 +1,5 @@
 variable "humber_id" {
-<<<<<<< HEAD
   default     = "n01698490"
-=======
-  default     = "n01698490"   
->>>>>>> a3b7f22c179740bb3355f0819181fdcdf16fdae3
 }
 
 variable "location" {
@@ -11,16 +7,6 @@ variable "location" {
 }
 
 variable "tags" {
-<<<<<<< HEAD
   type        = map(string)
   default     = {}
-=======
-  default = {
-    Assignment     = "CCGC 5502 Automation Assignment"
-    Name           = "Johnson.Joseph"
-    ExpirationDate = "2025-3-10"
-    Environment    = "Learning"
-  }
->>>>>>> a3b7f22c179740bb3355f0819181fdcdf16fdae3
 }
-
