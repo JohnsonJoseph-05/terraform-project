87341178c4c91fc0bd128ee74924d1ee4579ea0f
JohnsonJoseph-05 <johnsonmadathikunnel@gmail.com>
2025-03-10 13:32:43 -0400

    Update main.tf

--- modules/rgroup-n01698490/main.tf
+++ modules/rgroup-n01698490/main.tf
@@ -1,12 +1,5 @@
 resource "azurerm_resource_group" "rg_name" {
-<<<<<<< HEAD
-  name = "${var.humber_id}-RG"
-  location = var.location
-}
-=======
-  name     = "${var.humber_id}RG"  
+  name     = "${var.humber_id}RG"
   location = var.location
   tags     = var.tags
 }
-
->>>>>>> a3b7f22c179740bb3355f0819181fdcdf16fdae3
